fa3dd907c71f32c9800a2e0649add250904e2d61
snipercat <dagerenaq@gmail.com>
2014-04-20 15:31:45 -0500

    Attack Combox3, jump
    el personaje Salta y realiza un ataque en combo.

---
+++ Assets/Z_Test/Diego/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float distance = 3f;
+	public float height = 3f;
+	public float damping = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 wantedPosition = target.TransformPoint (0, height, -distance);
+
+		//transform.position = new Vector3( target.position.x, transform.position.y,  transform.position.z);
+
+
+		//transform.position = Vector3.Lerp (transform.position, wantedPosition, Time.deltaTime * damping);
+		Camera.main.transform.position = new Vector3 (target.gameObject.transform.position.x,
+		                                              target.gameObject.transform.position.y + height,
+		                                              Camera.main.transform.position.z
+		                                              );
+		                                              
+	}
+}
--- Assets/Z_Test/Diego/Scripts/ZeroControllerScript.cs
+++ Assets/Z_Test/Diego/Scripts/ZeroControllerScript.cs
@@ -3,66 +3,94 @@ using System.Collections;
 
 public class ZeroControllerScript : MonoBehaviour {
 
+
+
+	//Parameters
+	private Animator anim;
 	public float maxSpeed = 10f;
+	public LayerMask groundLayer;
+	public float jumpForce=210f;
+
+	//Status (Change to Private)
+	public bool grounded = false;
+	public bool attacking = false;
 	private bool facingRight = true;
 
-	private Animator anim;
 
-	public bool grounded = false;
-	public LayerMask groundLayer;
-
-
-	public AnimationState attack;
-
+//**************************************************
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 	}
-	
+//**************************************************	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float move = Input.GetAxis( "Horizontal");
+
+
+	//** HorizontalMovement
+		float move;
+		//Si esta atacando y esta en el piso se queda quieto
+		if( attacking && grounded)
+				move = 0;
+		else
+				move = Input.GetAxis( "Horizontal");
+
+		// Aplicar movimiento
 		rigidbody2D.velocity = new Vector2 (maxSpeed * move, rigidbody2D.velocity.y);
 		anim.SetFloat ("Speed", Mathf.Abs (move));
 
-		if (move > 0 && !facingRight)
+		// Si cambio la direccion del movimiento, voltear al personaje
+		if ( move > 0 && !facingRight)
 						Flip ();
 		else if (move < 0 && facingRight)
 						Flip ();
-
-		Camera.main.transform.position = new Vector3 (rigidbody2D.gameObject.transform.position.x,
-		                                              rigidbody2D.gameObject.transform.position.y,
-		                                              Camera.main.transform.position.z
-				);
-
 	}
-
-	void OnTriggerEnter2D(Collider2D otherCollider){
-		Flip ();
-		if (otherCollider.gameObject.layer == groundLayer)
-			grounded = true;
-						
-	}
-
+//**************************************************
 	void Update(){
+		
+	//*** Salto
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) {
+			//anim.SetBool("Ground", false);
+			rigidbody2D.AddForce(new Vector2(0, jumpForce));			
+		}
+		
 
-		if (Input.GetKeyDown (KeyCode.Z)) {
-						if (!anim.GetCurrentAnimatorStateInfo (0).IsTag ("Attack"))
-								anim.SetBool ("Attack", true);
-						anim.SetInteger ("AttackCount", anim.GetInteger ("AttackCount") + 1);
-				} else {
-						if (!anim.GetCurrentAnimatorStateInfo (0).IsTag ("Attack")) {
-								anim.SetBool ("Attack", false);
-								anim.SetInteger ("AttackCount", 0);
-						}
-				}
+	//*** Ataque
+		//SystemInfo esta en el suelo y se oprime Z, ataca y se aumenta uno al combo
+		if (Input.GetKeyDown (KeyCode.Z) && grounded ) {
 			
+			if (!anim.GetCurrentAnimatorStateInfo (0).IsTag ("Attack")){
+				attacking = true;
+				anim.SetBool ("Attack", true);
+			}
+			anim.SetInteger ("AttackCount", anim.GetInteger ("AttackCount") + 1);
+		}
+	}
+//**************************************************
+	void OnTriggerEnter2D(Collider2D otherCollider){
+		if (((1 << otherCollider.gameObject.layer) & groundLayer) != 0) {
+						grounded = true;
+						anim.SetBool ("Ground", true);
+				}
+	}
+//**************************************************
+	void OnTriggerExit2D(Collider2D otherCollider){
+		//Flip ();
+
+		if (((1 << otherCollider.gameObject.layer) & groundLayer) != 0) {
+						grounded = false;
+						anim.SetBool("Ground", false);
+				}
+		
 	}
 
+//**************************************************
 	void EndAttack(){
+		attacking = false;
 		anim.SetBool ("Attack", false);
+		anim.SetInteger ("AttackCount", 0);
 	}
-
+//**************************************************
 	void Flip(){
 		facingRight = !facingRight;
 		Vector3 newScale = transform.localScale;
