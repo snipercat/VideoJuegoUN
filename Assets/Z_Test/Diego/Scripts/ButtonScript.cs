1a673a9765f8f847296224134f729c615a9bfafb
Johan David Rodriguez Portela <jodrodriguezpo@unal.edu.co>
2014-05-19 00:31:19 -0500

    proyecto copia con movimientos para android
    Se modificio el script zeroController

---
+++ Assets/Z_Test/Diego/Scripts/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	void OnGUI () {
+		// Make a background box
+
+		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		if(GUI.Button(new Rect(20,400,80,80), "Saltar")) {
+			Application.LoadLevel(1);
+		}
+		
+		// Make the second button.
+		if(GUI.Button(new Rect(500,400,80,80), "Atacar")) {
+			Application.LoadLevel(2);
+		}
+	}
+
+}
---
+++ Copia para android/Assets/Z_Test/Diego/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float distance = 3f;
+	public float height = 3f;
+	public float damping = 5f;
+	public float horizontalOffset = 4f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Camera.main.transform.position = new Vector3 (target.gameObject.transform.position.x + horizontalOffset,
+		                                              Camera.main.transform.position.y,
+		                                              Camera.main.transform.position.z
+		                                              );
+		                                              
+	}
+}
